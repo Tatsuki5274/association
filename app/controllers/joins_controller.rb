299afe5f630148f7bc8b5246ed8b13de9fb83652
Tatsuki5274 <watanabe.tatsuki.infratop@gmail.com>
2019-01-07 07:39:26 +0000

    finish

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -2,5 +2,19 @@ class JoinsController < ApplicationController
 	def index
 	end
 	def new
+		@groups = Group.all
+	end
+
+	def create
+		gu = GroupUser.new
+		gu.user_id = current_user.id
+		gu.group_id = params[:group_id]
+		gu.save!
+		redirect_to joins_path
+	end
+	def destroy
+		gu = GroupUser.find_by(user_id: current_user.id, group_id: params[:id])
+		gu.destroy
+		redirect_to joins_path
 	end
 end
